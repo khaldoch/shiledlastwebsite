0ef916f862a89a11d38f23bdc3114d9b91a6223d
khaldoch <67787817+khaldoch@users.noreply.github.com>
2021-08-10 18:14:25 +0100

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function App() {
+  return (
+    <div className='bg-image hover-zoom' style={{ maxWidth: '22rem' }}>
+      <img src='https://mdbcdn.b-cdn.net/img/new/standard/city/053.jpg' className='w-100' />
+    </div>
+  );
+}
